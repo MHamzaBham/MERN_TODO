d4eb1a01ec56acde2198a63423220e2298ae31e5
Ali Bham <92208535+AliHamzaBham@users.noreply.github.com>
2024-04-05 02:15:37 +0500

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -15,7 +15,7 @@ app.use(cors({
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect("mongodb+srv://Hamza:alialibham@cluster01.njpdmew.mongodb.net/TodoApp")
+mongoose.connect("mongodb+srv://<Hamza>:<alialibham>@cluster01.njpdmew.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01") 
 
 const taskSchema = new mongoose.Schema({
     title: String,
